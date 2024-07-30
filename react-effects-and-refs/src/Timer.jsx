b778717f8425bdfb6f52762800638bfb932db81a
danx <rushn24@gmail.com>
2024-07-29 21:08:03 -0400

    updated Timer component to use useEffect

--- react-effects-and-refs/src/Timer.jsx
+++ react-effects-and-refs/src/Timer.jsx
@@ -1,10 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Timer = () => {
     const [seconds, setSeconds] = useState(0)
     // setInterval(() => {
     //     setSeconds(seconds => seconds + 1)
     // }, 1000)
+
+    // useEffect(() => {
+    //     setInterval(() => {
+    //         setSeconds(seconds => seconds + 1);
+    //     }, 1000);    
+    // }, []);
     return (
         <h1>{seconds}</h1>
     );
